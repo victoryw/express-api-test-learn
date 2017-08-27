0f57113b46cd7199bb0df0f29b84c1b64d3bd7c3
tianyawy <tianya_wy@hotmail.com>
2017-08-27 21:56:21 +0800

    [vicotryw] #n/a add the json test

--- test/app.test.js
+++ test/app.test.js
@@ -20,13 +20,22 @@ const proxiedMongoose = {
 const app = proxyQuire('../bin/www', { mongoose: proxiedMongoose });
 
 const userModel = require('../module/user');
+
 describe('test/app.test.js', ()=>{
     it('should test happy path', (done)=>{
         let user = new userModel({name: 'aa', email: 'bb'});
         user.save().then(
             request(app).get(`/users/${user.id}`)
                 .expect(200)
-                .then(()=>{done();}));
+                .then((res)=>
+                {
+                    res.body.email.should.eql(user.email);
+                    res.body.name.should.eql(user.name);
+                    done();
+                })
+                .catch((error) => {
+                    done(error);
+                }))
     })
 });
 
